18f0fe26b24d371e1563ab90a9697e673db47930
GMosquera1 <genesismosquera@pursuit.org>
2018-10-05 16:29:37 -0400

    Initial Commit

---
+++ testNumber5000/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  testNumber5000
+//
+//  Created by Genesis Mosquera on 10/5/18.
+//  Copyright © 2018 Genesis Mosquera. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
